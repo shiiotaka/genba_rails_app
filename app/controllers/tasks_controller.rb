86e1a59e2e084c82a8422d8d84202e2bc18cddba
shiiotaka <shiiotaka@gmail.com>
2021-01-27 20:44:50 +0900

    log

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,7 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
     if @task.save
+      logger.debug "task: #{@task.attributes.inspect}"
       redirect_to tasks_path, notice: "タスク#{@task.name}を保存しました"
     else
       render 'new'
